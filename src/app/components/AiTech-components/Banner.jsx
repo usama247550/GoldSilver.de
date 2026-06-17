c13c9e376f3234b9e52c5485dad0de42375263b5
Usama-javed24 <usamajaved@arswift.com>
2026-06-17 16:01:54 +0500

    fix banner ai-tech page

--- src/app/components/AiTech-components/Banner.jsx
+++ src/app/components/AiTech-components/Banner.jsx
@@ -12,8 +12,7 @@ const Banner = () => {
           alt="image"
           className="w-full h-56  sm:h-72 md:h-96 object-cover rounded-2xl md:rounded-3xl"
         />
-
-        <div className=" absolute inset-0 bg-linear-to-r from-[#111111] to-[#66666600] rounded-2xl  md:rounded-3xl" />
+        <div className="absolute inset-0 bg-linear-to-r from-black/40 to-transparent rounded-2xl md:rounded-3xl" />
 
         <div className="absolute inset-0 flex flex-col justify-center gap-y-2 md:gap-y-3 ps-5 sm:ps-8 md:ps-10 pr-5 sm:pr-[45%]">
           <div className="bg-[#F7BD48] text-[#000000] rounded-sm text-xs sm:text-sm text-center py-1 w-fit px-3">
@@ -22,11 +21,10 @@ const Banner = () => {
           <h1 className="text-white font-[Playfair_Display] font-bold text-xl sm:text-3xl md:text-4xl leading-snug">
             {t("AI, Semiconductors")} <br /> {t("& Tech-Industrial Demand")}
           </h1>
-          <p className="text-white  w-[125%]  text-xs sm:text-sm md:text-lg leading-relaxed">
+          <p className="text-white  w-[105%]  text-xs sm:text-sm md:text-md leading-relaxed">
             {t(
               "Track how AI infrastructure, semiconductor manufacturing, and advanced technologies are increasing industrial demand for precious metals.",
             )}
-            
           </p>
         </div>
       </div>
