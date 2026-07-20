bbf33bf996a013c354691424e1eee141549e950b
Usama-javed24 <usamajaved@arswift.com>
2026-07-20 11:19:53 +0500

    remove commission from affiliate card according to sheet

--- src/app/components/marco-Components/PartnerCard.jsx
+++ src/app/components/marco-Components/PartnerCard.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useTranslation } from "react-i18next";
 import React from "react";
-
 const PartnerCard = () => {
   const { t } = useTranslation();
   const partnerCards = [
@@ -10,7 +9,6 @@ const PartnerCard = () => {
       title: "Seeking Alpha",
       description:
         "Buy investment-grade gold and silver products from one of Germany's established bullion retailers.",
-      commission: "Up to 6% commission",
       button: "Discover More",
       link: process.env.NEXT_PUBLIC_AFFILIATE_SEEKINGALPHA,
     },
@@ -19,7 +17,6 @@ const PartnerCard = () => {
       title: "Motley Fool",
       description:
         "Precious metals investment solutions designed to help investors diversify and protect wealth.",
-      commission: "Up to 8-10% commission",
       button: "Discover More",
       link: process.env.NEXT_PUBLIC_AFFILIATE_MOTLEYFOOL,
     },
@@ -28,47 +25,46 @@ const PartnerCard = () => {
       title: "Research Newsletters",
       description:
         "Purchase, store, and manage physical gold and silver through a globally trusted platform.",
-      commission: "25% recurring commission",
       button: "Discover More",
       link: process.env.NEXT_PUBLIC_AFFILIATE_RESEARCH,
     },
   ];
-
   return (
-    <section id="Investment"  className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+    <section
+      id="Investment"
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto"
+    >
+      {" "}
       {partnerCards.map((item, index) => (
         <div
           key={index}
           className="border border-[#B8860B] px-4 py-6 text-center flex flex-col gap-y-4 rounded-2xl shadow-sm"
         >
+          {" "}
           <span className="bg-[#0D1B2A] font-[Playfair_Display]  w-fit mx-auto px-4 py-2 text-white rounded-md text-sm">
-            {item.logo}
-          </span>
-
+            {" "}
+            {item.logo}{" "}
+          </span>{" "}
           <div className="text-[#000000] text-xl sm:text-2xl font-bold font-[Playfair_Display] ">
-            {t(item.title)}
-          </div>
-
+            {" "}
+            {t(item.title)}{" "}
+          </div>{" "}
           <div className="text-[#000000] text-sm flex-1 max-w-2lg">
-            {t(item.description)}
-          </div>
-
-          <div className="text-[#B8860B] font-medium text-sm">
-            {t(item.commission)}
-          </div>
-
+            {" "}
+            {t(item.description)}{" "}
+          </div>{" "}
           <a
             href={item.link || "#"}
             target="_blank"
             rel="noopener noreferrer"
             className="mt-2 text-[#B8860B] border-2 border-[#B8860B] rounded-md text-sm font-bold py-2 hover:bg-[#B8860B] hover:text-black transition cursor-pointer block text-center"
           >
-            {t(item.button)}
-          </a>
+            {" "}
+            {t(item.button)}{" "}
+          </a>{" "}
         </div>
-      ))}
+      ))}{" "}
     </section>
   );
 };
-
 export default PartnerCard;
